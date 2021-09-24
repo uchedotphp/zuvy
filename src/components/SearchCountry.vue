eefceb717278efbc12c320448db7fedd7813ddbb
Nwulu Uchechukwu Prince <uchedotphp@gmail.com>
2021-09-24 16:59:53 +0100

    Search and Filter components built

---
+++ src/components/SearchCountry.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="relative rounded-md shadow-sm overflow-hidden">
+      <div
+        class="
+          absolute
+          inset-y-0
+          left-0
+          pl-8
+          flex
+          items-center
+          pointer-events-none
+        "
+      >
+        <img
+          v-if="isDarkThemeOn"
+          class="h-5 lg:h-4"
+          src="../assets/images/icons/search-dark.svg"
+          alt="a half white moon"
+        />
+        <img
+          v-else
+          class="h-5 lg:h-4"
+          src="../assets/images/icons/search-light.svg"
+          alt="a half dark moon"
+        />
+      </div>
+      <input
+        :class="'focus:outline-none block w-full pl-20 pr-10 py-5 text-lg text-gray-500 shadow-xl bg-whiteColor border border-gray-400 border-opacity-20 rounded-md dark:bg-darkBlueColor font-medium dark:text-veryLightGrayColor dark:placeholder-veryLightGrayColor placeholder-gray-300'"
+        placeholder="Search for a country..."
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "SearchCountry",
+  computed: {
+    ...mapGetters({ isDarkThemeOn: "isDarkThemeOn" }),
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/regionFilterDropdown.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="relative w-3/4 lg:w-48">
+    <button
+      @click="showDropDown = !showDropDown"
+      type="button"
+      class="
+        bg-white
+        dark:bg-darkBlueColor
+        dark:border-transparent
+        dark:text-veryLightGrayColor
+        relative
+        w-full
+        border border-gray-300
+        rounded-md
+        shadow-sm
+        pl-6
+        pr-10
+        py-5
+        text-left text-lg
+        focus:outline-none
+        sm:text-sm
+      "
+      aria-haspopup="listbox"
+      aria-expanded="true"
+      aria-labelledby="listbox-label"
+    >
+      <span class="block truncate"> Filter by Region </span>
+      <span
+        class="
+          absolute
+          inset-y-0
+          right-0
+          flex
+          items-center
+          pr-4
+          pointer-events-none
+        "
+      >
+        <img
+          v-if="isDarkThemeOn"
+          class="h-3"
+          src="../assets/images/icons/chevronDownDark.svg"
+          alt="arrow pointing down"
+        />
+        <img
+          v-else
+          class="h-3"
+          src="../assets/images/icons/chevron-down-light.svg"
+          alt="arrow pointing down"
+        />
+      </span>
+    </button>
+    <transition
+      leave-active-class="transition ease-in duration-100"
+      leave-from-class="opacity-100"
+      leave-to-class="opacity-0"
+    >
+      <ul
+        v-if="showDropDown"
+        class="
+          absolute
+          z-10
+          mt-2
+          w-full
+          bg-white
+          dark:bg-darkBlueColor
+          shadow-lg
+          max-h-60
+          rounded-md
+          py-1
+          text-base
+          ring-1 ring-black ring-opacity-5
+          overflow-auto
+          focus:outline-none
+        "
+        role="listbox"
+        aria-labelledby="listbox-label"
+        aria-activedescendant="listbox-option-3"
+      >
+        <li
+          v-for="n in 5"
+          :key="n"
+          class="
+            dark:text-veryLightGrayColor
+            dark:hover:text-darkBlueColor
+            dark:hover:bg-veryLightGrayColor
+            hover:bg-darkBlueColor hover:text-whiteColor
+            capitalize
+            text-gray-900
+            cursor-pointer
+            select-none
+            relative
+            py-2
+            px-6
+          "
+          id="listbox-option-0"
+          role="option"
+        >
+          <span class="font-normal block truncate"> Africa </span>
+        </li>
+      </ul>
+    </transition>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "RegionFilterSelect",
+  data() {
+    return {
+      showDropDown: false,
+    };
+  },
+  computed: {
+    ...mapGetters({ isDarkThemeOn: "isDarkThemeOn" }),
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
