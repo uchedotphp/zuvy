8ae6996129937e0e8b4b6920e28031d245d2a5ff
Nwulu Uchechukwu Prince <uchedotphp@gmail.com>
2021-09-25 05:47:27 +0100

    [Larger & Smaller Screen Views]: Country Listings

---
+++ src/components/countriesList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    role="list"
+    class="
+      grid grid-cols-1
+      gap-14
+      sm:gap-8
+      md:gap-10
+      sm:grid-cols-2
+      md:grid-cols-3
+      lg:grid-cols-4
+    "
+  >
+    <single-country-card v-for="n in 10" :key="n" />
+  </div>
+</template>
+
+<script>
+import SingleCountryCard from "./singleCountryCard.vue";
+export default {
+  name: "SingleCountry",
+  components: {
+    SingleCountryCard,
+  },
+};
+</script>
+SingleCountryCard
+
+<style lang="scss" scoped></style>
---
+++ src/components/singleCountryCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <ul>
+    <li
+      class="
+        col-span-1
+        flex flex-col
+        bg-white
+        dark:bg-darkBlueColor
+        rounded-lg
+        overflow-hidden
+        shadow
+        divide-y divide-gray-200
+        max-w-xs
+      "
+    >
+      <div class="flex-1 flex flex-col">
+        <img
+          class="flex-shrink-0"
+          src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+          alt=""
+        />
+        <div class="px-4 pt-6 pb-10 dark:text-veryLightGrayColor">
+          <h3
+            class="
+              text-gray-900
+              dark:text-veryLightGrayColor
+              text-lg
+              font-medium
+            "
+          >
+            united states of america
+          </h3>
+          <dl class="mt-1 flex-grow flex flex-col space-y-2">
+            <dd class="text-sm">
+              <span class="font-semibold">Population</span>: 323,947,000
+            </dd>
+            <dd class="text-sm">
+              <span class="font-semibold">Region</span>: Americas
+            </dd>
+            <dd class="text-sm">
+              <span class="font-semibold">Capital</span>: Washington, D.C
+            </dd>
+          </dl>
+        </div>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "SingleCountryCard",
+};
+</script>
+
+<style lang="scss" scoped></style>
